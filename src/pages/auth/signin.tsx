a6dfa3c2028ddff8bd30c1a119cff7be777d7fc3
marius florescu <mariusflorescu23@icloud.com>
2022-10-25 12:49:10 +0300

    feat: auth layout (#1)
    * auth layout
    
    * sign out instead of sign in

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,5 +1,6 @@
 import { GetServerSidePropsContext, NextPage } from 'next'
-import { getProviders, signIn, useSession } from 'next-auth/react'
+import { getProviders, signIn } from 'next-auth/react'
+import Head from 'next/head'
 
 type Provider = {
   name: string
@@ -11,26 +12,35 @@ type Props = {
 }
 
 const SignIn: NextPage<Props> = ({ providers }) => {
-  const session = useSession()
   return (
-    <div>
-      <h1>Sign In page</h1>
-      {Object.values(providers).map((provider) => {
-        return (
-          <div key={provider.name}>
-            <button
-              onClick={() =>
-                signIn(provider.id, {
-                  callbackUrl: `${window.location.origin}/app/overview`
-                })
-              }
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      <Head>
+        <title>FitBuddy - Sign In</title>
+      </Head>
+      <div className="flex h-full w-full flex-col items-center justify-center px-8">
+        <div className="flex w-full flex-col items-center space-y-8 rounded-lg bg-white p-8 shadow">
+          <h1 className="text-lg font-bold">FitBuddy</h1>
+          <h2 className="text-2xl font-semibold">Welcome!</h2>
+          <span className="h-[1px] w-full bg-gray-100" />
+          {Object.values(providers).map((provider) => {
+            return (
+              <div key={provider.name}>
+                <button
+                  className="bg-gray-900 font-medium text-white"
+                  onClick={() =>
+                    signIn(provider.id, {
+                      callbackUrl: `${window.location.origin}/app/overview`
+                    })
+                  }
+                >
+                  Sign in with {provider.name}
+                </button>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/auth/signout.tsx
+++ src/pages/auth/signout.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
 import { signOut, useSession } from 'next-auth/react'
 
 const SignOut: NextPage = () => {
@@ -7,7 +8,27 @@ const SignOut: NextPage = () => {
   if (status === 'authenticated') {
     signOut({ redirect: false })
   }
-  return <div>You have been logged out</div>
+
+  return (
+    <>
+      <Head>
+        <title>FitBuddy - Sign Out</title>
+      </Head>
+      <div className="flex h-full w-full flex-col items-center justify-center px-8">
+        <div className="flex w-full flex-col items-center space-y-8 rounded-lg bg-white p-8 shadow">
+          <h1 className="text-lg font-bold">FitBuddy</h1>
+          <h2 className="text-2xl font-semibold">You've been logged out</h2>
+          <p className="text-sm text-gray-400">
+            We hope to see you again soon!
+          </p>
+          <span className="h-[1px] w-full bg-gray-100" />
+          <button className="w-full bg-gray-900 text-gray-50">
+            Go back to login
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default SignOut
